6309444dcae9f9e9799e73c0d01ad6e2d8c5ebcb
zhanhb <zhanhb88@gmail.com>
2016-08-14 19:33:04 +0800

    Ensure cache works

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 if git config --get remote.origin.url 2>&1 | grep -F github.com/zhanhb/ckfinder-spring-boot -q && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   mvn -Duser.name=zhanhb -DskipTests=true clean deploy -s .travis/settings.xml $*
-  exit $?
 else
   mvn install -B
-  exit $?
 fi
 
