5eea8b95ff9ebc43fa2f8063ef938e96b4aae3ff
zhanhb <zhanhb88@gmail.com>
2016-09-09 16:32:18 +0800

    Enable testing

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if git config --get remote.origin.url 2>&1 | grep -F github.com/zhanhb/ckfinder-spring-boot -q && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  mvn -Duser.name=zhanhb -DskipTests=true clean deploy -s .travis/settings.xml $*
+  mvn -B -Duser.name=zhanhb clean deploy -s .travis/settings.xml $*
 else
-  mvn install -B
+  mvn -B -Duser.name=zhanhb install $*
 fi
 
